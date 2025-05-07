3fd693aa2528e185775b237535c5617f96a589c4
BioHome <bigjarik@gmail.com>
2025-05-08 00:24:36 +0300

    New page work around

--- ViewModels/SubModulesVM.cs
+++ ViewModels/SubModulesVM.cs
@@ -11,9 +11,21 @@ namespace HollyJson.ViewModels
     public class SubModulesVM
     {
         CommandHandler _unlocktags;
-        CommandHandler _setdurationtime;
         CommandHandler _switchcodex;
         CommandHandler _unlockbuildparams;
+        CommandHandler _setxpmultiplayer;
+        CommandHandler _setresduration;
+        CommandHandler _settechduration;
+
+        private bool isX1Chosen;
+        private bool isX2Chosen;
+        private bool isX5Chosen;
+        private bool isX10Chosen;
+
+        public bool IsX1Chosen { get => isX1Chosen; set => isX1Chosen = value; }
+        public bool IsX2Chosen { get => isX2Chosen; set => isX2Chosen = value; }
+        public bool IsX5Chosen { get => isX5Chosen; set => isX5Chosen = value; }
+        public bool IsX10Chosen { get => isX10Chosen; set => isX10Chosen = value; }
         public string PathToConfDir { get; set; }
         //FROM CONF FILES
         public ObservableCollection<TechInfo> Techs { get; set; }
@@ -21,6 +33,45 @@ namespace HollyJson.ViewModels
         public ObservableCollection<Building> Buildings { get; set; }
         public Dictionary<string, string> GameVarLst { get; set; }
         public ObservableCollection<CharXp> CharXps { get; set; }
+
+        public string ResearchSpd_curr
+        {
+            get
+            {
+                if (Techs.Count > 0)
+                {
+                    var q = Techs.Select(t => t.duration);
+                    if (q.Any(t => t != 0))
+                    {
+                        return "Default";
+                    }
+                    else
+                        return "Modified";
+                }
+                else
+                    return string.Empty;
+            }
+        }
+        public int ResearchPreset { get; set; }
+        public string TechhSpd_curr
+        {
+            get
+            {
+                if (GameVarLst.Count > 0)
+                {
+                    int a = int.Parse(GameVarLst["tech_creation_days_per_point_below_average"]);
+                    int b = int.Parse(GameVarLst["tech_creation_days_per_point_average"]);
+                    int c = int.Parse(GameVarLst["tech_creation_days_per_point_above_average"]);
+                    if (a + b + c == 600)
+                        return "Default";
+                    else
+                        return "Modified";
+                }
+                else
+                    return string.Empty;
+            }
+        }
+        public int TechPreset { get; set; }
         //FROM state JSON
         public DateTime Now { get; set; }
         public ObservableCollection<string> TagBank { get; set; }
@@ -31,6 +82,7 @@ namespace HollyJson.ViewModels
         public bool HaveActivePolicy { get; set; }
         public string NameOfActivePolicy { get; set; }
         public bool? isCodexOpened { get; set; }
+
 
         public SubModulesVM()
         {
@@ -72,18 +124,27 @@ namespace HollyJson.ViewModels
                 }, (obj) => PathToConfDir.Length > 0);
             }
         }
-        public CommandHandler SetDurationTimeTech
+        public CommandHandler SetTechDurationCmd
         {
             get
             {
-                return _setdurationtime ??= new CommandHandler(obj =>
+                return _settechduration ??= new CommandHandler(obj =>
+                {
+                    GameVarLst["tech_creation_days_per_point_below_average"] = 
+                    GameVarLst["tech_creation_days_per_point_average"] = 
+                    GameVarLst["tech_creation_days_per_point_above_average"] = TechPreset.ToString();
+                }, (obj) => true);
+            }
+        }
+        public CommandHandler SetResDurationCmd
+        {
+            get
+            {
+                return _setresduration ??= new CommandHandler(obj =>
                 {
                     foreach (var item in Techs)
-                    {
-                        item.duration = (int)obj;
-                    }
-                    return;
-                }, (obj) => true);
+                        item.duration = ResearchPreset;
+                }, (obj) => Techs.Count > 0);
             }
         }
         public CommandHandler UnlockBuildsParamsCmd
@@ -117,7 +178,36 @@ namespace HollyJson.ViewModels
                 }, (obj) => true);
             }
         }
-        public async Task ReadBuildingsData(string path)
+        public CommandHandler SetXpMultCmd
+        {
+            get
+            {
+                return _setxpmultiplayer ??= new CommandHandler(obj =>
+                {
+                    if (obj is not null)
+                        foreach (var item in CharXps)
+                        {
+                            switch (obj as string)
+                            {
+                                case "1":
+                                    item.SetXpMod(1); //ТУТ дефолт ставить..
+                                    break;
+                                case "2":
+                                    item.SetXpMod(2.0d);
+                                    break;
+                                case "5":
+                                    item.SetXpMod(5.0d);
+                                    break;
+                                case "10":
+                                    item.SetXpMod(10.0d);
+                                    break;
+                            }
+                        }
+                }, (obj) => true);
+            }
+        }
+
+        public async Task ReadPerksData(string path)
         {
             JObject jobj = null;
             string jsonstr = await File.ReadAllTextAsync(path);
